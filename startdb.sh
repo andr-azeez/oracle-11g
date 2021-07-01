c9d3d47685495a679d9d3db3bf41678cc0244267
azeez <azeez.cahcet@gmail.com>
2021-07-01 16:06:22 +0000

    updated startdb.sh

--- startdb.sh
+++ startdb.sh
@@ -1,5 +1,11 @@
 #!/bin/bash 
 
+cp -R -n /u01/app/oracle_bkp/* /u01/app/oracle
+chown -R oracle:dba /u01/app/oracle
+chmod -R 6751 /u01/app/oracle
+
+# cp -R -n /u01/app/oracle_bkp/* 
+
 sqlplus /nolog << EOF 
 CONNECT SYS/oracle AS SYSDBA; 
 startup; 
